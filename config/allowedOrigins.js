e40a9a774cf873f55bfb5a2d653eae5d9148a773
triadonoss <triadonoss@gmail.com>
2024-02-24 04:56:37 +0800

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:8081'
+    'http://localhost:8081',
+    'https://dashboarddemoversion.netlify.app'
 ];
 
 module.exports = allowedOrigins;
