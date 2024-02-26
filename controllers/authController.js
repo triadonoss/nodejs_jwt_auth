b3867e15996ee60ab0312d0e7fdf9e8d795651b8
triadonoss <triadonoss@gmail.com>
2024-02-26 13:26:20 +0800

    24 hrs
    24 hours expiration

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,12 +23,12 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: null }
+            { expiresIn: '24h' }
         );
         const newRefreshToken = jwt.sign(
             { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: null }
+            { expiresIn: '24h' }
         );
 
         // Changed to let keyword
